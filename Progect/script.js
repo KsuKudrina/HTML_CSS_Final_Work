c45e00d373a14d543bce0cdb729b19857782bc4a
Ksu <ksenia.barteva@gmail.com>
2024-02-29 16:32:00 +0700

    home work seminar 10

--- Progect/script.js
+++ Progect/script.js
@@ -1,16 +1,4 @@
-// var myHeading = document.querySelector("h1");
-// myHeading.textContent = "Hello world!";
 
-// document.documentElement.style.setProperty('--count', 5);
-
-
-// var element = document.getElementById('myElement');
-// var oldValue = element.getAttribute('style');
-// var newValue = oldValue.replace('--my-var: старое значение', '--my-var: новое значение');
-// element.setAttribute('style', newValue);
-
-// var sheet = document.styleSheets[0];
-// sheet.cssRules[0].style.setProperty('--count', 'новое значение');
 
 var clicks = 0;
 function onClick() {
